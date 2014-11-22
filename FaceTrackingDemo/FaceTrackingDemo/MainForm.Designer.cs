8b853f8da2f5bb6c850b0efb76edadb2bd6c0125
Shinya Fujie <sfujie@gmail.com>
2014-11-22 17:59:32 +0900

    カメラから画像をキャプチャして表示する部分を実�

--- FaceTrackingDemo/FaceTrackingDemo/MainForm.Designer.cs
+++ FaceTrackingDemo/FaceTrackingDemo/MainForm.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelDateTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // labelDateTime
+            // 
+            this.labelDateTime.AutoSize = true;
+            this.labelDateTime.Location = new System.Drawing.Point(12, 501);
+            this.labelDateTime.Name = "labelDateTime";
+            this.labelDateTime.Size = new System.Drawing.Size(109, 12);
+            this.labelDateTime.TabIndex = 1;
+            this.labelDateTime.Text = "2014/01/01 01:01:01";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(744, 522);
+            this.Controls.Add(this.labelDateTime);
+            this.Controls.Add(this.pictureBox);
             this.Name = "MainForm";
             this.Text = "Face Tracking Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelDateTime;
     }
 }
 
--- FaceTrackingDemo/FaceTrackingDemo/MainForm.cs
+++ FaceTrackingDemo/FaceTrackingDemo/MainForm.cs
@@ -12,9 +12,28 @@ namespace FaceTrackingDemo
 {
     public partial class MainForm : Form
     {
+        private VideoCameraCapture videoCameraCapture = null;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.videoCameraCapture = new VideoCameraCapture();
+            this.videoCameraCapture.VideoCameraFrameCaptured += 
+                videoCameraCapture_VideoCameraFrameCaptured;
+
+            this.videoCameraCapture.Start();
+        }
+
+        void videoCameraCapture_VideoCameraFrameCaptured(object sender, 
+            VideoCameraFrameCapturedEventArgs e)
+        {
+            this.pictureBox.Image = 
+            OpenCvSharp.Extensions.BitmapConverter.ToBitmap(
+                e.Image, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            this.pictureBox.Invalidate();
+
+            this.labelDateTime.Text = e.DateTime.ToString("HH:mm:ss.fff");
         }
     }
 }
